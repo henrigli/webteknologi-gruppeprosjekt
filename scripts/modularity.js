587d7cf33e0adcda248867059d2b7926bcc2daaa
henrigli <henrigli@stud.ntnu.no>
2021-09-21 14:32:16 +0200

    first commit

---
+++ scripts/modularity.js
@@ -0,0 +1,12 @@
+let bodyEl = document.querySelector('body');
+
+bodyEl.innerHTML += '<header id="navbar">'
+                    + '<p id="joy">Joy of Picturing</p>'
+                    + '<p id="about" class="navbutton"><a href="about.html">About & Contact</a></p>'
+                    + '<p id="prizes" class="navbutton"><a href="prices.html">Prices</a></p>'
+                    + '<p id="gallery" class="navbutton"><a href="gallery.html">Gallery</a></p>'
+                    + '<p id="home" class="navbutton"><a href="index.html">Home</a></p>'
+                    + '</header>'
+                    + '<footer>'
+                    + '<p>&#9400; Copyright 2019. All rights reserved.</p>'
+                    + '</footer>';
